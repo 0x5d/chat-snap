6797affc4fd8a99d2e3998b40df3e69d6a647321
David Castillo <castillobgr@gmail.com>
2016-09-11 17:19:27 -0500

    Broadcast. Add start script

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ app.use(express.static('public'));
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 io.on('connection', function (socket) {
+  socket.join('chat');
   socket.on('file', function (file) {
-    socket.emit('file', file);
+    socket.broadcast.to('chat').emit('file', file);
   });
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
