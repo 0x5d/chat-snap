02aee7bad865129ad65581a047e639e2af7dc2bc
David Castillo <castillobgr@gmail.com>
2016-09-12 22:31:09 -0500

    Add Modernizr. Refactor

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var express = require('express');
-
 var app = express();
 app.use(express.static('public'));
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+
 io.on('connection', function (socket) {
   socket.join('chat');
   socket.on('file', function (file) {
--- public/index.js
+++ public/index.js
@@ -1,31 +1,41 @@
-// Grab elements, create settings, etc.
-var video = document.getElementById('video');
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+(function(){// Grab elements, create settings, etc.
+  let video = document.getElementById('video');
+  let canvas = document.getElementById('canvas');
+  let context = canvas.getContext('2d');
 
-var socket = io.connect();
+  let socket = io.connect();
 
-// Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  // Not adding `{ audio: true }` since we only want video now
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+  // Get access to the camera!
+  if(Modernizr.getusermedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(initVideo)
+      .catch(handleError)
+  } else {
+    alert("Sorry, your browser doesn't support video :(")
+  }
+
+  function initVideo (stream) {
     video.src = window.URL.createObjectURL(stream);
     video.play();
-  });
-}
+  }
 
-// Trigger photo take
-document.getElementById("snap").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 640, 480);
-  canvas.toBlob(function (blob) {
-    socket.emit('file', blob);
+  function handleError (err) {
+    alert("Something happened. Try again, maybe? :)")
+  }
+
+  // Trigger photo
+  document.getElementById("snap").addEventListener("click", function() {
+    context.drawImage(video, 0, 0, 640, 480);
+    canvas.toBlob(function (blob) {
+      socket.emit('file', blob);
+    });
   });
-});
 
 
-socket.on('file', function (arrayBuffer) {
-  var blob = new Blob([arrayBuffer])
-  createImageBitmap(blob).then(function(image){
-	  context.drawImage(image, 0, 0, 320, 240);
+  socket.on('file', function (arrayBuffer) {
+    var blob = new Blob([arrayBuffer])
+    createImageBitmap(blob).then(function(image){
+      context.drawImage(image, 0, 0, 320, 240);
+    });
   });
-});
+})()
