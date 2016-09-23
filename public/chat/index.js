e000d2b41a628d2358fb188d1225386d6cb03dee
David Castillo <castillobgr@gmail.com>
2016-09-22 20:57:06 -0500

    Refactor

--- public/chat/index.js
+++ public/chat/index.js
@@ -1,39 +1,45 @@
 $(function () {
   'use strict'
 
-  const id = getChatId()
-  if (id === '') {
-    window.location = '/'
-  }
+  let socket
 
-  let socket = io.connect()
-  socket.emit('enter', id)
+  window.location.search.replace(/[?&]+id=([^&]*)/, (m, id) => {
+    if (id === '') {
+      window.location = '/'
+      return
+    }
 
-  // Get access to the camera!
-  const options = { video: true }
-  // Try to use latest API.
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia(options)
-      .then(initVideo)
-      .catch(handleError)
-  } else {
-    // Fall back to deprecated API if available.
-    navigator.getUserMedia = navigator.getUserMedia || 
-                             navigator.webkitGetUserMedia || 
-                             navigator.mozGetUserMedia
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia(options, initVideo, handleError)
+    socket = io.connect()
+    socket.on('file', processFile)
+    socket.emit('enter', id)
+    initVideo()
+    // Trigger photo
+    $('#snap').on('click', snap)
+  })
+
+
+  function initVideo () {
+    // Get access to the camera!
+    const options = { video: true }
+    // Try to use latest API.
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(options)
+        .then(initPreview)
+        .catch(handleError)
     } else {
-      alert("Sorry, your browser doesn't support video :(")
+      // Fall back to deprecated API if available.
+      navigator.getUserMedia = navigator.getUserMedia || 
+                              navigator.webkitGetUserMedia || 
+                              navigator.mozGetUserMedia
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(options, initPreview, handleError)
+      } else {
+        alert("Sorry, your browser doesn't support video :(")
+      }
     }
   }
 
-  // Trigger photo
-  $('#snap').on('click', snap)
-
-  socket.on('file', processFile)
-
-  function initVideo (stream) {
+  function initPreview (stream) {
     let video = document.getElementById('video')
     video.src = window.URL.createObjectURL(stream)
     video.play()
@@ -66,30 +72,22 @@ $(function () {
   }
 
   function renderImageElement (canvas, source) {
-    let row = $('<div></div>')
-    row.addClass('row')
+    let $row = $('<div></div>')
+    $row.addClass('row')
 
-    let imgContainer = $('<div></div>')
-    imgContainer.addClass('small-4 columns ' + source)
-    imgContainer.appendTo(row)
+    let $imgContainer = $('<div></div>')
+    $imgContainer.addClass(`small-4 columns ${source}`)
+    $imgContainer.appendTo($row)
 
-    $(canvas).appendTo(imgContainer)
+    $(canvas).appendTo($imgContainer)
 
     if (source === 'me') {
-      let fill = $('<div></div>')
-      fill.addClass('small-8 columns')
-      fill.prependTo(row)
+      let $fill = $('<div></div>')
+      $fill.addClass('small-8 columns')
+      $fill.prependTo($row)
     }
-    row.appendTo('#feed')
-    row.get(0).scrollIntoView()
-  }
-
-  function getChatId() {
-    let id = ''
-    window.location.search.replace(/[?&]+id=([^&]*)/, function (m, value) {
-      id = value
-    })
-    return id
+    $row.appendTo('#feed')
+    $row.get(0).scrollIntoView()
   }
 
   function handleError (err) {
