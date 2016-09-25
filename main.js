f0d62e73bae3980fa87b1dcb06c81b05de5fe051
David Castillo <castillobgr@gmail.com>
2016-09-25 12:31:43 -0500

    Refactor

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-io.on('connection', handleConnection)
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
-
 app.post('/chat', (req, res) => {
   const sessionId = sentencer.make('{{ adjective }}-{{ noun }}')
   res.redirect(`/chat/?id=${sessionId}`)
 })
+
+io.on('connection', handleConnection)
 
 server.listen(PORT, () => console.log(`chat-snap is listening on port ${PORT}`))
 
